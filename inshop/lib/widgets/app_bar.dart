017f77cdff40e8ddced08ef22dda99980afdcf1d
Morten Msiska <bsc-05-20@unima.ac.mw>
2024-10-09 01:19:00 -0700

    Implemented the custom_search_delegate.dart and modified appbar class

--- inshop/lib/widgets/app_bar.dart
+++ inshop/lib/widgets/app_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:test_demo/widgets/custom_search_delegate.dart';
+import 'package:inshop/widgets/custom_search_delegate.dart';
 
 class AppBarScreen extends StatelessWidget implements PreferredSizeWidget {
   @override
