0de46111ac1e3ae7bf9c8d7a412d847ae1bec2da
Morten Msiska <bsc-05-20@unima.ac.mw>
2024-10-09 01:27:56 -0700

    Changed the layout of the whole main_app

---
+++ inshop/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'Cart.dart';
+import 'widgets/app_bar.dart';
+import 'widgets/Bottom_navigation.dart';
+import 'widgets/product.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBarScreen(),
+        body: const Padding(
+          padding: EdgeInsets.all(16.0), // Add padding to the body
+          child: SingleChildScrollView(
+            child: Wrap(
+              spacing: 20.0, // Horizontal space between items
+              runSpacing: 20.0, // Vertical space between rows
+              children: [
+                //CartScreen(),
+               // ProductScreen(),
+                // Add more ProductScreen widgets as needed
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationScreen(),
+      ),
+    );
+  }
+}
